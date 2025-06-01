56c3e591cbd6b0028caf5481ee66f1308e29ab98
lonesurv1vor <14860837+lonesurv1vor@users.noreply.github.com>
2025-06-01 12:07:58 +0200

    Improve logic, include delivery and side quests

--- worlds/cavesofqud/__init__.py
+++ worlds/cavesofqud/__init__.py
@@ -1,9 +1,8 @@
 from typing import List
 
-from BaseClasses import Entrance, LocationProgressType
+from BaseClasses import Entrance, LocationProgressType, Region
 from worlds.AutoWorld import WebWorld, World
 
-from . import Rules
 from . import Items
 from . import Locations
 from . import Options
@@ -29,34 +28,17 @@ class CoQWorld(World):
     item_name_to_id = {item: id for id, item in enumerate(Items.all_items, 1)}
     location_name_to_id = {name: id for id, name in enumerate(Locations.all_locations, 1)}
 
+    # World gen calls
+
     def fill_slot_data(self) -> dict:
         return self.options.as_dict(*[name for name in self.options_dataclass.type_hints.keys()])
 
-    def generate_early(self):
-        return
-    
     def create_regions(self):
         Regions.create_regions(self)
 
     def create_items(self):
-        item_pool: List[Items.CoQItem] = []
-        total_locations = len(self.multiworld.get_unfilled_locations(self.player))
-
-        # Main quest unlock items
-        for quest in Quests.main_quests(self):
-            item_name = Quests.quest_unlock_item(quest)
-            item_pool += [Items.CoQItem(item_name, Items.ItemClassification.progression, self.item_name_to_id[item_name], self.player)]
-
-        # Stat ups from level ups
-        for level in Regions.levelup_levels(Quests.max_level(self)):
-            item_pool += Items.create_stat_items_on_levelup(self, level)
-
-        # Fill up with fillers
-        while len(item_pool) < total_locations:
-            item_pool += [Items.create_filler_item(self)]
-
-        self.multiworld.itempool += item_pool
+        Items.create_items(self)
 
     def set_rules(self):
-
-        Rules.set_rules(self)
+        victory_loc = self.get_location(Quests.goal_lookup[self.options.goal])
+        self.multiworld.completion_condition[self.player] = lambda state: victory_loc in state.locations_checked
